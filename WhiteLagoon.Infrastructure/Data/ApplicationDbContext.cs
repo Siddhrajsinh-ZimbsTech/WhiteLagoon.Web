d174086e9386ab0b33bc8fd243e10d716687d539
Siddhrajsinh-ZimbsTech <siddhrajsinh.zimbstech@gmail.com>
2025-03-31 18:17:44 +0530

    Add Create and update UI

---
+++ WhiteLagoon.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WhiteLagoon.Domain.Entites;
+
+namespace WhiteLagoon.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Villa> Villas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder); // Uncomment this to avoid potential issues
+            
+            modelBuilder.Entity<Villa>().HasData(  // Fixed typo: HashData → HasData
+                new Villa
+                {
+                    Id = 1,
+                    Name = "Royal Villa",
+                    Description = "Fusce tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                    ImageUrl = "https://placehold.co/600x400",
+                    Occupancy = 4,
+                    Price = 200,
+                    Sqft = 550,
+                },
+                new Villa
+                {
+                    Id = 2,
+                    Name = "Premium Pool Villa",
+                    Description = "Fusce tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                    ImageUrl = "https://placehold.co/600x401",
+                    Occupancy = 4,
+                    Price = 300,
+                    Sqft = 550,
+                },
+                new Villa
+                {
+                    Id = 3,
+                    Name = "Luxury Pool Villa",
+                    Description = "Fusce tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                    ImageUrl = "https://placehold.co/600x402",
+                    Occupancy = 4,
+                    Price = 400,
+                    Sqft = 750,
+                }
+            );
+        }
+    }
+}
---
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Domain.Entites;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace WhiteLagoon.Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VillaController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var Villas = _context.Villas.ToList();
+            return View(Villas);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Villa obj)
+        {
+            if (obj.Name == obj.Description)
+            {
+                ModelState.AddModelError("Name", "The description cannot exactly match the Name. ");
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Villas.Add(obj);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+
+        public IActionResult Update(int VillaId)
+        {
+            Villa? obj = _context.Villas.FirstOrDefault(x => x.Id == VillaId);
+            if (obj == null)
+            {
+                return RedirectToAction("Error","Home");
+            }
+            return View(obj);
+        }
+    }
+}
